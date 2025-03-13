45698283b88db80619acc4ce652bd04b54b7a4b9
Mathu1015 <unora4281@gmail.com>
2025-03-13 11:46:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,7 +88,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', 'Leyonpublic') # Public
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/LeyonRequeast')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/LeyonAutoMovies')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/World4kMovie')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 't.me/LeyonRequeast') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Leomsle')
